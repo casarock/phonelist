880d2f42d82eed1f92f72f76e31842b4985268ea
mdix <dix@mediaman.de>
2012-02-24 21:37:08 +0100

    Added tweetsized template engine (mir.aculo.us)

--- js/tweettemplate.js
+++ js/tweettemplate.js
@@ -1,7 +1,8 @@
 // Big thanks to Thomas Fuchs for this one.
 // http://mir.aculo.us/2011/03/09/little-helpers-a-tweet-sized-javascript-templating-engine/
 function doTemplate(s,d){
- for(var p in d)
-   s=s.replace(new RegExp('{'+p+'}','g'), d[p]);
- return s;
+    for (var p in d) {
+        s=s.replace(new RegExp('{'+p+'}','g'), d[p]);
+    }
+    return s;
 }
