25cd4f36a969fa1e5939a00c62e1a26b3567bb33
mdix <dix@mediaman.de>
2012-02-28 17:01:58 +0100

    Some styling & a elems to enable hover via tap on iPhone

--- js/phonelist.js
+++ js/phonelist.js
@@ -13,41 +13,41 @@ var PhoneList = {};
 PhoneList.data = {};
 PhoneList.view = {};
 
-// builds app after data has been loaded
-PhoneList.data.ready = function() {
-    domready(function () {
-        PhoneList.view.fillContactList();
-    })
-}
-
 // renders contact list elements
 PhoneList.view.fillContactList = function() {
     var contEl = document.getElementById('contacts');
     if ('object' == typeof contEl) {
         var i = PhoneList.data.contacts.length;
         while (i--) {
-            contEl.innerHTML += doTemplate('<span><span>{vorname} {nachname}</span>' +
+            contEl.innerHTML += doTemplate('<span><a href="#">{vorname} {nachname}' +
                                            '<span class="detail">{abteilung}</span>' +
                                            '<span class="detail">{durchwahl}</span>' +
                                            '<span class="detail">{nummer}</span>' +
                                            '<span class="detail">{handy}</span>' +
-                                           '<span class="detail">{skype}</span>' +
+                                           '<span class="detail">{skype}</span></a>' +
                                            '</span>', PhoneList.data.contacts[i]);
         }
     }
 }
 
-// Loads data from localStorage or via XHR - calls ready() once data is ready
-PhoneList.data.retrieveContacts = (function() {
+PhoneList.data.retrieveContacts = function() {
     var contacts = localStorage.getItem('contacts');
 
     if (null !== contacts) {
         PhoneList.data.contacts = JSON.parse(contacts);
+        PhoneList.view.fillContactList();
+        return;
     } else {
         microAjax("mock/phoneMock.json", function (res) {
             PhoneList.data.contacts = JSON.parse(res);
             localStorage.setItem('contacts', res);
+            PhoneList.view.fillContactList();
         });
+        return;
     }
-    PhoneList.data.ready();
-})();
+};
+
+// Loads data from localStorage or via XHR - calls ready() once data is ready
+domready(function () {
+    PhoneList.data.retrieveContacts();
+});
